513981476d64d6e62b783e015c24e8fde6e383b3
bikboktech <bikboktech@gmail.com>
2023-09-03 15:18:48 +0200

    feat: creates get playlist endpoint

--- source/spotify/routes.js
+++ source/spotify/routes.js
@@ -1,10 +1,12 @@
 import express from "express";
+import getPlaylist from "./getPlaylist/index.js";
 import getTracks from "./getTracks/index.js";
 
 const SERVICE_NAME = "spotify";
 
 const router = express.Router();
 
+router.get(`/${SERVICE_NAME}/playlists/:playlistId`, getPlaylist);
 router.get(`/${SERVICE_NAME}/tracks`, getTracks);
 
 export default router;
