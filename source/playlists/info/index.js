cc830f06caebdf6fbaa4396b6e868d130e9abf64
bikboktech <bikboktech@gmail.com>
2023-10-21 15:15:26 +0200

    playlist info fix

--- source/playlists/info/index.js
+++ source/playlists/info/index.js
@@ -3,16 +3,36 @@ import knex from "../../common/data/database.js";
 const PLAYLISTS_TABLE = "playlists";
 
 const getPlaylistInfo = async (request, response) => {
-  const playlist = await knex(PLAYLISTS_TABLE)
-    .where("playlists.id", request.params.playlistId)
-    .first();
+  const { context, params } = request;
 
-  response.status(200).json({
-    id: playlist.id,
-    spotifyPlaylistId: playlist.spotify_playlist_id,
-    playlistName: playlist.name,
-    // playlistNotes: playlist.notes,
-  });
+  let playlist;
+
+  if (params.eventId) {
+    playlist = await knex(PLAYLISTS_TABLE)
+      .where("playlists.event_id", params.eventId)
+      .first();
+  } else {
+    playlist = await knex(PLAYLISTS_TABLE)
+      .where("playlists.id", params.playlistId)
+      .first();
+  }
+
+  if (!playlist) {
+    response.status(200).json({});
+  } else {
+    const playlistOutput = await getSpotifyPlaylist(
+      playlist.spotify_playlist_id,
+      context.spotifyToken
+    );
+
+    response.status(200).json({
+      id: playlist.id,
+      spotifyPlaylistId: playlist.spotify_playlist_id,
+      name: playlist.name,
+      // playlistNotes: playlist.notes,
+      tracks: playlistOutput.tracks,
+    });
+  }
 };
 
 export default getPlaylistInfo;
