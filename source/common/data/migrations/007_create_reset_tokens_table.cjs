1eafff23b2aac552fe09c22ded3a6a5974a654bf
andhrelja <andhrelja@hotmail.com>
2024-02-03 23:04:43 +0100

    fix: rename reset_tokens_table

---
+++ source/common/data/migrations/007_create_reset_tokens_table.cjs
@@ -0,0 +1,12 @@
+exports.up = async (knex) => {
+  await knex.schema.createTable("reset_tokens", (table) => {
+    table.integer("account_id").unsigned().notNullable();
+    table.varchar("hash", 512).notNullable();
+    table.integer("expiry_time").defaultTo(86400); // 24 hours
+    table.dateTime("created_at", 6).notNullable().defaultTo(knex.fn.now(6));
+  });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTable('tokens');
+};
