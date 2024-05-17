9c521a1da8cd72f2694c7a7ea461676364fae7bc
andhrelja <andhrelja@hotmail.com>
2024-05-17 17:40:42 +0200

    perf: update seed records

--- source/common/data/seeds/002_insert_dj_records.cjs
+++ source/common/data/seeds/002_insert_dj_records.cjs
@@ -3,35 +3,35 @@ exports.seed = async knex => {
         {
             account_type_id: 2, 
             full_name: 'DJ Luca Montecchi (Disco Daddy)', 
-            email: 'luca-montecchi@musicology.com', 
+            email: 'luca-montecchi@musicology.pro', 
             password: '$2a$12$R7FAJFvQsHSV1yksyES/jOSMA5Lxyd1zRqJY5Dvd8GO1vVRStUNii', 
             active: 1
         },
         {
             account_type_id: 2, 
             full_name: 'DJ Li\'l Cox', 
-            email: 'lil-cox@musicology.com', 
+            email: 'lil-cox@musicology.pro', 
             password: '$2a$12$R7FAJFvQsHSV1yksyES/jOSMA5Lxyd1zRqJY5Dvd8GO1vVRStUNii', 
             active: 1
         },
         {
             account_type_id: 2, 
             full_name: 'DJ Balkan Boy', 
-            email: 'balkan-boy@musicology.com', 
+            email: 'balkan-boy@musicology.pro', 
             password: '$2a$12$R7FAJFvQsHSV1yksyES/jOSMA5Lxyd1zRqJY5Dvd8GO1vVRStUNii', 
             active: 1
         },
         {
             account_type_id: 2, 
             full_name: 'DJ Manuel Lupen', 
-            email: 'manuel-lupen@musicology.com', 
+            email: 'manuel-lupen@musicology.pro', 
             password: '$2a$12$R7FAJFvQsHSV1yksyES/jOSMA5Lxyd1zRqJY5Dvd8GO1vVRStUNii', 
             active: 1
         },
         {
             account_type_id: 2, 
             full_name: 'DJ Peruc Beats', 
-            email: 'peruc-beats@musicology.com', 
+            email: 'peruc-beats@musicology.pro', 
             password: '$2a$12$R7FAJFvQsHSV1yksyES/jOSMA5Lxyd1zRqJY5Dvd8GO1vVRStUNii', 
             active: 1
         },
